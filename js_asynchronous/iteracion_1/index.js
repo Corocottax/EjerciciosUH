bafb96aa258046d9c39824d818faf2522e4754e1
unknown <santicorocottax@gmail.com>
2021-12-20 19:46:14 +0100

    He acabado todos los ejercicios

---
+++ js_asynchronous/iteracion_1/index.js
@@ -0,0 +1,55 @@
+// 1.1
+fetch("https://api.agify.io?name=michael").then((res) => res.json())
+.then((myRes) => {
+
+    console.log(myRes);
+
+});
+
+// 2
+const btn$$ = document.querySelector("button");
+const getInput$$ = document.querySelector("input");
+
+btn$$.addEventListener("click", consulta);
+
+function consulta() {
+
+    fetch("https://api.nationalize.io/?name=" + getInput$$.value).then((res) =>
+    res.json().then((myRes) => {
+
+        // 2.2
+        console.log(myRes); 
+        
+        // 2.3 y 2.4
+        console.log(myRes);       
+
+        console.log(myRes.country.length);
+
+        for (i = 0; i < myRes.country.length; i++) {
+
+            const p$$ = document.createElement("p");
+
+            p$$.textContent = `El usuario ${myRes.name} tiene ${Math.floor((myRes.country[i].probability)*100)}% de ser ${myRes.country[i].country_id}`;
+            document.body.appendChild(p$$);
+
+            const pButton$$ = document.createElement("button");
+
+            pButton$$.textContent = "x";
+
+            pButton$$.addEventListener("click", () => borrar(p$$, pButton$$));
+
+            document.body.appendChild(pButton$$);
+
+        }
+        
+        }));
+
+}
+
+function borrar(nodoABorrar, boton) {
+    
+    nodoABorrar.remove();
+    boton.remove();
+
+}
+
--- js_es6/iteracion_7/index.js
+++ js_es6/iteracion_7/index.js
@@ -12,17 +12,38 @@ const exams = [
     {name: 'Keanu Reeves', score: 10}
 ];
 
-const suma = exams.reduce(examen => {
-    
-    return examen + examen.score});
+const suma = exams.reduce((acumulador, exam) => acumulador + exam.score, 0);
 
 console.log(suma);
 
 
 // 7.2
+const aprobados = exams.reduce((acumulador, exam) => {
+    
+    let sumaAprobados = 0;
 
+    if (exam.score >= 5) {
+
+        sumaAprobados = acumulador + exam.score;
+        
+    } else {
+
+        return acumulador;
+
+    }
+
+    return sumaAprobados;
+
+}, 0);
+
+console.log(aprobados);
 
 
 // 7.3
 
+const total = exams.reduce((acumulador, exam) => acumulador + exam.score, 0);
+
+const media = total / exams.length;
+
+console.log(media);
 
